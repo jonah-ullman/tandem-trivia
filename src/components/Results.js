ca78c113e4143b09913bbb2665d792e59913707e
Jonah Ullman <jonahmullman@gmail.com>
2020-10-28 17:58:11 -0500

    Addressing bug with final question not properly updating score

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function Results({ result, setContent }) {
+  const playCount = localStorage.getItem('playCount');
+  const totalScore = localStorage.getItem('totalScore');
+  const average = Math.round(totalScore / playCount);
   return (
     <div id="results">
       <h2>You answered {result} out of 10 questions correctly.</h2>
+      <h3>You've played a total of {playCount} rounds.</h3>
+      <h3>Your average score is {average}/10</h3>
       <button type="button" className="button" onClick={setContent}>
         Play Again?
       </button>
